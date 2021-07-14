893acb08455e0d71cbf814144b571457a882f917
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-07-14 22:22:16 +0900

    initial commit

---
+++ apps/image-resize-kun/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'image-resize-kun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'image-resize-kun';
+}
