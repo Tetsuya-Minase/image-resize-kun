3ba453dc9fc6ebd5d76938c9f11fbfc1976d853a
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-07-15 23:06:47 +0900

    feat: add header component

--- apps/image-resize-kun/src/app/app.module.ts
+++ apps/image-resize-kun/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HeaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
